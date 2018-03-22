a26149a06f40a628f774d0a41116d43776fb91c6
vignesh071 <vignesh.0791@gmail.com>
2018-03-22 12:21:29 +0530

    initial setup

---
+++ src/org/gitpract/Vikidemo.java
@@ -0,0 +1,15 @@
+package org.gitpract;
+
+public class Vikidemo {
+	public void demo1() 
+	{
+		
+    System.out.println("testing git");
+	}
+public static void main(String[] args) {
+	
+	Vikidemo obj = new Vikidemo();
+	obj.demo1();
+	
+}
+}
